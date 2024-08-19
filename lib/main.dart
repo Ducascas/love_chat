d5f5ebc4a57766ccadf13b1cfc7edad02ca39685
o`neill <100170314+Ducascas@users.noreply.github.com>
2024-08-20 00:53:36 +0400

    Update main.dart
    delete WidgetsFlutterBinding.ensureInitialized()

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:love_chat/love_chat_app.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const LoveChatApp());
 }
